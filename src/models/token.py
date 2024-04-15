b61f958c6fa0b7346c92ee7694019bd182049f79
devonwarren <devon.warren@gmail.com>
2024-04-15 17:19:06 -0400

    wip

--- src/models/token.py
+++ src/models/token.py
@@ -1,67 +1,43 @@
-from pydantic import BaseModel, Field
-from enum import Enum
-from models.crd import CustomResource, CustomResourceDefinitionAdditionalPrinterColumn
+from typing import Literal, Optional
+from pydantic import (
+    BaseModel,
+    Field,
+    PastDatetime,
+    AwareDatetime,
+    SecretStr,
+    PrivateAttr,
+)
+from models.crd import CRDMeta, CRDPrinterColumn
 
 
-# the type of export type to save token into
-class ExportTypes(str, Enum):
-    SECRET = "Secret"
+class TokenStatus(BaseModel):
+    ready: Optional[bool] = None
+    last_rotated: Optional[PastDatetime] = None
+    expiration: Optional[AwareDatetime] = None
 
 
-class RotationStrategies(str, Enum):
-    IMMEDIATE = "Immediate"
-    KEEP_OLD = "KeepOld"
+class Token(BaseModel):
+    "A generic token type"
 
+    name: str = Field(description="The name of the token in k8s")
+    value: SecretStr = Field(description="The secret value of the token")
+    # type: Literal['Secret'] = 'Secret'
+    rotation_schedule: Optional[str] = Field(
+        alias="rotationSchedule", description="A CronTab text representing when to run"
+    )
+    force_now: Optional[bool] = Field(
+        default=False,
+        alias="forceNow",
+        description="Set to true to force a token refresh now",
+    )
+    status: Optional[TokenStatus | dict]
 
-# the export info for how the token gets saved
-class Export(BaseModel):
-    Type: ExportTypes
-    Name: str
-    Namespace: str
-    Annotations: dict[str, str] = Field(default={})
-
-class TokenSpec(BaseModel):
-    name: str
-    value: str
-    type: str
-    rotation_schedule: str = Field(alias="rotationSchedule")  # Crontab text definition
-    force_now: bool = Field(default=False, alias="forceNow")
-
-
-# the definition of the token to rotate
-class TokenCRD(
-    CustomResource,
-    scope="Namespaced",
-    group="token-rotator.org",
-    names={
-        "kind": "Token",
-        "plural": "tokens",
-        "singular": "token",
-        "listKind": "TokenList",
-        "shortNames": [],
-        "categories": ["all", "token"],
-    },
-    additionalPrinterColumns=[
-        CustomResourceDefinitionAdditionalPrinterColumn(
-            name="Status",
-            type="string",
-            description="If the token is currently valid without any issues",
-            jsonPath='.status.conditions[?(@.type=="Ready")].status',
-        ),
-        CustomResourceDefinitionAdditionalPrinterColumn(
-            name="Expiration",
-            type="date",
-            description="A timestamp of when this token is set to expire",
-            jsonPath=".status.expirationTimestamp",
-        ),
-        CustomResourceDefinitionAdditionalPrinterColumn(
-            name="Last Rotated",
-            type="date",
-            description="A timestamp of when this token was last rotated",
-            jsonPath=".status.lastRotated",
-        ),
-    ],
-):
-    spec: TokenSpec
-    # RotationStrategy: RotationStrategies = Field(default=RotationStrategies.IMMEDIATE)
-    # Export: Export
+    # internal attributes
+    _crd_meta = CRDMeta(
+        scope="Namespaced",
+        group="token-rotator.org",
+        kind="Token",
+        singular="token",
+        plural="tokens",
+        list_kind="TokenList",
+    )
